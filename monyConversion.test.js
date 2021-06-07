202b420f7bb5798b3e5011384678964a48522bef
Jizga <jizga93@gmail.com>
2021-06-07 18:05:29 +0000

    All test have only the expect line

--- monyConversion.test.js
+++ monyConversion.test.js
@@ -3,25 +3,20 @@ const { fromEuroToDollar, fromDollarToYen, fromYanToPound } = require('./monyCon
 
 test("One euro should be 1'206 dollars", () => {
 
-    const dollars = fromEuroToDollar(3.5)
-
-    const expected = 3.5 * 1.2;
-
-    expect(expected).toBe(dollars)
+    expect(fromEuroToDollar(3.5)).toBe(4.2)
 })
 
 test("One dollar should to be 106'583 yens => 127.9/1.2", () => {
 
-    const dollars = fromDollarToYen(15);
-    const expected = (15 * 127.9) / 1.2;
-
-    expect(expected).toBe(dollars)
+    expect(fromDollarToYen(15)).toBe(1598.75)
 })
 
 test("One yen should to be  0,00625 british pound =>  0.8/127.9", () => {
 
-    const yen = fromYanToPound(800);
-    const expected = (800 * 0.8) / 127.9
-
-    expect(expected).toBe(yen)
+    expect(fromYanToPound(800)).toBe(5.003909304143862)
 })
+
+// test("One dollar should to be 0,667 british pound => 0.8/1.2", () => {
+//     const dollar = fromDollarToYen(1)
+//     const yens = fromYanToPound(1)
+// })
